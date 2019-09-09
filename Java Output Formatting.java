b7fa689c0c73256ee6f8df616509d097e323389c
shreyasshree <44720590+shreyasshree@users.noreply.github.com>
2019-09-09 16:35:35 +0530

    Create Java Output Formatting.java
    for output formating program in java refer this program it is complete

---
+++ Java Output Formatting.java
@@ -0,0 +1,15 @@
+# my_-contribution
+import java.util.Scanner;
+public class Solution {
+public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+System.out.println(&quot;================================&quot;);
+for(int i=0;i&lt;3;i++)
+{
+String s1=sc.next();
+int x=sc.nextInt();
+System.out.printf(&quot;%-15s%03d%n&quot;, s1, x);
+}
+System.out.println(&quot;================================&quot;);
+}
+}
