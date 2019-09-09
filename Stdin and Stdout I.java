c1335c71699f2bd858327f5a79c7c72f3f71324f
shreyasshree <44720590+shreyasshree@users.noreply.github.com>
2019-09-09 16:31:26 +0530

    Create Stdin and Stdout I.java
    for stdin prorams in java

---
+++ Stdin and Stdout I.java
@@ -0,0 +1,14 @@
+# my_-contribution
+import java.util.*;
+public class Solution {
+public static void main(String[] args) {
+Scanner scan = new Scanner(System.in);
+int[] a= new int[3]; //creating array
+for (int i=0;i&lt;3;i++)
+//Assigning values to array from stdin
+a[i] = scan.nextInt();
+scan.close();
+for(int j=0;j&lt;3;j++)
+//Printing array values to stdout
+System.out.println(a[j])
+}
