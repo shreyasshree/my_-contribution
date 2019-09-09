6ea9a6be5930641a0cd03a79265dc85726ae6204
shreyasshree <44720590+shreyasshree@users.noreply.github.com>
2019-09-09 16:33:44 +0530

    Create Stdin and Stdout II.java
    for sdin 2 in java

---
+++ Stdin and Stdout II.java
@@ -0,0 +1,14 @@
+# my_-contribution
+import java.io.*;
+public class Solution {
+public static void main(String[] args) throws IOException{
+BufferedReader scan = new BufferedReader(new
+InputStreamReader(System.in));
+int i = Integer.parseInt(scan.readLine());
+double d = Double.parseDouble(scan.readLine());
+String s = scan.readLine();
+System.out.println(&quot;String: &quot; + s);
+System.out.println(&quot;Double: &quot; + d);
+System.out.println(&quot;Int: &quot; + i);
+}
+}
